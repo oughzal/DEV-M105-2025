4ea13f13a2f3e53a59127e8200b9f858499cbdb8
omar OUGHZAL <omar.oughzal@gmail.com>
2025-03-08 04:00:20 +0000

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Types de données
+let pi = 3.14; // 1. Number
+let greeting = "Hello, world!"; // 2. String
+let isStudent = true;// 3. Boolean
+let numbers = [1, 2, 3, 4, 5];// 4. Array
+let person = { firstName: "John",lastName: "Doe",age: 30 };// 5. Object
+let sum = function(a, b) {return a + b;};// 6. Function
+let emptyValue = null;// 7. Null
+let notAssigned;// 8. Undefined
+let uniqueId = Symbol("id");// 9. Symbol
+let bigNumber = BigInt(123456789012345678901234567890);// 10. BigInt
